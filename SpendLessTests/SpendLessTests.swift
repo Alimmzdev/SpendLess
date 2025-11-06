624b433754bae5778d54e22e5dfe17f0f9029fd3
Ali Mohammadzadeh <ali24Mohammadzadeh@gmail.com>
2025-11-06 17:15:55 +0330

    Initial Commit

---
+++ SpendLessTests/SpendLessTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpendLessTests.swift
+//  SpendLessTests
+//
+//  Created by Alimmz on 11/6/25.
+//
+
+import Testing
+
+struct SpendLessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
